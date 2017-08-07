4f322d0980cf553e0a755f99d809e838375ab751
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-08-07 14:32:48 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Yamini."
+  end
+  get '/hometown' do
+    "My hometown is Jaipur."
+  end
+  get '/favorite-song' do
+    "My favorite song is Fried Rice by G-Eazy."
+  end
 end
